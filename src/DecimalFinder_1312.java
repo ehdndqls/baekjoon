ddaffb2410fad6ddd1bc866d19add4821839eced
ehdndqls <zxv9530@gmail.com>
2025-04-03 21:13:26 +0900

    25.03.24(2)

---
+++ src/DecimalFinder_1312.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DecimalFinder_1312 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numerator = sc.nextInt();
+        int denominator = sc.nextInt();
+        int decimalPlace = sc.nextInt();
+
+
+    }
+}
