96e5f81dc937c3dd80a29dd3ec34d4ac142d41cc
ehdndqls <zxv9530@gmail.com>
2025-04-04 20:21:54 +0900

    25.04.04

--- src/DecimalFinder_1312.java
+++ src/DecimalFinder_1312.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
 
 public class DecimalFinder_1312 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 입력 받기
         int numerator = sc.nextInt();
         int denominator = sc.nextInt();
         int decimalPlace = sc.nextInt();
 
+        sc.close();
 
+        int remainder = numerator % denominator;
+
+        // N번째 소수점 자리 찾기
+        for (int i = 0; i < decimalPlace; i++) {
+            remainder *= 10;  // 자리 이동
+            int digit = remainder / denominator; // 현재 자리 숫자
+            remainder %= denominator; // 새로운 나머지 업데이트
+
+            if (i == decimalPlace - 1) {
+                System.out.println(digit);
+            }
+        }
     }
 }
